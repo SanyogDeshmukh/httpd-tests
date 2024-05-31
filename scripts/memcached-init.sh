932339e798ef603f1e38f389c7027e0bdb4dcb6e
SanyogDeshmukh <72301054+SanyogDeshmukh@users.noreply.github.com>
2024-05-31 20:31:08 +0530

    Update memcached-init.sh

--- scripts/memcached-init.sh
+++ scripts/memcached-init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 DOCKER=${DOCKER:-`which docker 2>/dev/null || which podman 2>/dev/null`}
-${DOCKER} build -t httpd_memcached - <<EOF
+sudo ${DOCKER} build -t httpd_memcached - <<EOF
 FROM quay.io/centos/centos:stream9
 RUN yum install --setopt=retries=5 --setopt=timeout=30 -y memcached
 CMD /usr/bin/memcached -u memcached -v
